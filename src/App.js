2f7ae1db8419c71cee06d0cdccb001cafe992e4a
ginatorres <gina.bjork@my.metrostate.edu>
2021-10-23 12:58:53 -0500

    Started implementation for dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import './App.css';
 import React, { Component } from 'react';
 import Users from './components/users';
+import background from './images/Background.png'; 
+import secondaryImage from './images/Type.png'; 
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
     state = {
         users: []
     }
-
-    componentDidMount() {
-        fetch('http://localhost:8080/user/getusers')
-        .then(res => res.json())
-        .then((data) => {
-        this.setState({ users: data })
-        })
-        .catch(console.log)
-    }
+      
+    // componentDidMount() {
+    //     fetch('http://localhost:8080/user/getusers')
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //     this.setState({ users: data })
+    //     })
+    //     .catch(console.log)
+    // }
 
     render() {
         return (
-            <Users users={this.state.users} />
+            <div>
+                <div class="container">      
+                    <Button class="LoginButton">
+                        Log in
+                    </Button>
+                    <Button class="SignupButton">
+                        Sign Up
+                    </Button>
+                    <Button class="SoloplayButton">
+                        Solo Play
+                    </Button>
+                    <Button class="GroupplayButton">
+                        Group Play
+                    </Button>
+                    <img src={background}/>
+                </div>
+                <div>
+                    <img src={secondaryImage} style={{height:'auto',width:'35%'}}/>
+                    <h5>WHAT IS TYPINGGAME?</h5>
+                    <h2>TypingGame is the most effective way to improve typing skills.</h2>
+                </div>
+            </div>
+                
+
+            // <Users users={this.state.users} />
         )
     }
 }
