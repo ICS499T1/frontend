6a6f78feb3daed3cccc55520d366c38e4fc15f7b
ginatorres <gina.bjork@my.metrostate.edu>
2021-10-24 17:56:49 -0500

    fixed header

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,7 @@ import { styled } from '@mui/material/styles';
 import Button from "@material-ui/core/Button";
 // import background from './images/Background.png'; 
 import secondaryImage from '../images/Type.png'; 
+import background from '../images/Background.png'; 
 import Grid from '@mui/material/Grid';
 import { Typography } from "@material-ui/core";
 import Container from '@mui/material/Box';
@@ -18,19 +19,20 @@ const Img = styled('img')({
 const Body = () => {
     return (
         <React.Fragment>
-            <header className="bg-image">
+            <header>
                 <div>
+                    <img class="imgcontainer" src={background}/>
                     <Button class="soloplay">Solo Play</Button>
                     <Button class="multiplay">Multi Play</Button>
                 </div>
-            </header> 
+            </header>
             <Container class="container">
                 <Grid container alignItems="center">
                     <Grid item>
                         <Img  src={secondaryImage}/>
                     </Grid>
                     <Grid item xs={12} sm container>
-                        <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs container direction="column">
                             <Grid item xs>
                                 <Typography gutterBottom variant="subtitle1" component="div">
                                     <div>
