63aaa4c5bc2285087c286350bb929909cb8f029c
ginatorres <gina.bjork@my.metrostate.edu>
2021-10-25 15:54:57 -0500

    Dashboard is now responsive

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ import Box from '@mui/material/Box';
 
 const Img = styled('img')({
     maxWidth: '400px',
-    marginRight: '100px',
+    // marginRight: '100px',
   });
   
 const Body = () => {
@@ -26,37 +26,54 @@ const Body = () => {
                     <Button class="multiplay">Multi Play</Button>
                 </div>
             </header>
-            <Container class="container">
-                <Grid container alignItems="center">
-                    <Grid item>
+            <body>
+            <Box class="whitebox">
+                <Grid container columnSpacing={5}  rowSpacing={5} justify="flex-start">
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
                         <Img  src={secondaryImage}/>
                     </Grid>
-                    <Grid item xs={12} sm container>
-                        <Grid item xs container direction="column">
-                            <Grid item xs>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                            <Grid item>
                                     <div>
-                                        <h4 class="textone">WHAT IS TYPINGGAME?</h4>
+                                        <p class="textone">WHAT IS TYPINGGAME?</p>
                                         <h3 class="text">TypingGame is the most effective way to improve typing skills.</h3>
                                         <Button class="startnowbutton">Get Started Now</Button>
                                     </div>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Container>
-  
-            <Box sx={{ flexGrow: 1 }} class="box">
-                <Grid container spacing={2} columns={16}>
-                    <Grid item xs={8}>
-                        <h3 class="textBarone">Individual and Group Stats</h3>
-                    </Grid>
-                    <Grid item xs container direction="column" spacing={5}>
-                        <Grid item xs={8}>
-                            <h3 class="textBartwo">Interactive Experience</h3>
                         </Grid>
                     </Grid>
                 </Grid>
             </Box>
+
+            <Box class="boxone">
+                <Grid container columnSpacing={5}  justify="flex-start">
+                    <Grid item xs={12} sm={12} md={4} lg={4} textAlign="center">
+                        <p class="textBar">2+ Million Users</p>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4} order={{ xs: 2, sm: 3 }} textAlign="center">
+                        <p class="textBar">Individual and Group Races</p>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4} order={{ xs: 2, sm: 3 }} textAlign="center">
+                        <p class="textBar">Interactive Experience</p>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <Box class="boxtwo">
+                <Grid container columnSpacing={5}  rowSpacing={5} justify="flex-start">
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <h2 class="subtitle">How do I get started?</h2>
+                        <p>Keep practicing until you get all five starts, it really doesn't take much to learn, 
+                            a few minutes a day for one or two weeks and you will be a pro!</p>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <h2 class="subtitle">Do I need an account?</h2>
+                        <p>You don't need to create an account. You can play solo or multiplayer games anonimously. 
+                            However, you might consider getting an account if you would like to keep track of your results.</p>
+                    </Grid>
+                </Grid>
+            </Box>
+       
+            </body>
         </React.Fragment>
     )
 }
